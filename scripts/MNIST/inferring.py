8d8854adddba641cb13718dce7101835f5fa9f00
xiangnan <xiangnan0718@gmail.com>
2021-11-08 16:35:21 +0800

    增加MNIST模型推理，验证模型

---
+++ scripts/MNIST/inferring.py
@@ -0,0 +1,30 @@
+import os
+
+from torchvision import transforms
+from torch import load, no_grad
+from PIL import Image
+
+from model import MNISTModel
+
+
+# 实例化模型
+model = MNISTModel()
+# 加载模型
+model.load_state_dict(load('./models/mnist_last_model.pkl'))
+# 加载数据集
+transform = transforms.Compose([
+    transforms.Grayscale(1),
+    transforms.ToTensor(),
+    transforms.Normalize((0.1307,), (0.3081,))
+])
+
+image = Image.open('scripts/MNIST/7.jpg')
+image = transform(image)
+
+model.eval()
+
+# 推理
+with no_grad():
+    output = model(image)
+    result = output.max(dim=1).indices
+    print(result.item())
