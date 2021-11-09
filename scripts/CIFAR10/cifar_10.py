91c1e4ee4179b8c2d8c297b45aa0d98034854793
xiangnan0718 <xiangnan0718@gmail.com>
2021-11-09 17:19:17 +0800

    采用模型集成方式优化CIFAR10数据集，准确率未提升，原因未知

--- scripts/CIFAR10/cifar_10.py
+++ scripts/CIFAR10/cifar_10.py
@@ -1,5 +1,6 @@
 # encoding=utf-8
 import os
+from copy import deepcopy
 
 from tqdm import tqdm
 import numpy as np
@@ -26,6 +27,28 @@ def check_accuracy(model, loader, device):
     # 计算准确率
     accuracy = np.mean(succeed)
     return accuracy
+
+
+def multi_check_accuracy(models, loader, device):
+    total_correct = []
+    model_correct = [[] for _ in range(len(models))]
+    loader = tqdm(loader, total=len(loader))
+    with torch.no_grad():
+        for img, label in loader:
+            img, label = img.to(device), label.to(device)
+            for index, model in enumerate(models):
+                model.eval()
+                out = model(img)
+                result = out.max(dim=1).indices
+                result_mean = result.eq(label).float().mean()
+                model_correct[index].append(result_mean.item())
+
+    for index, model in enumerate(models):
+        accuracy = np.mean(model_correct[index])
+        print(f'{model.__class__.__name__} 模型的准确率为：{accuracy*100:.2f}%')
+        total_correct.append(accuracy)
+
+    return np.mean(total_correct)
 
 
 def train(epochs, model, train_loader, valid_loader, optimizer, criterion, device):
@@ -70,6 +93,42 @@ def train(epochs, model, train_loader, valid_loader, optimizer, criterion, devic
     return model
 
 
+def multi_train(epochs, models, train_loader, valid_loader, optimizer, criterion, device):
+    best_accuracy = 0
+    best_models = [None for _ in range(len(models))]
+    for epoch in range(1, epochs+1):
+        # 动态修改参数学习率
+        if epoch % 5 == 0:
+            optimizer.param_groups[0]['lr'] *= 0.1
+        # epoch_loss
+        epoch_loss = []
+        # 训练模型
+        train_loader = tqdm(train_loader, total=len(train_loader))
+        for images, labels in train_loader:
+            # 指定运行环境
+            images, labels = images.to(device), labels.to(device)
+            # 清空梯度
+            optimizer.zero_grad()
+            # 多模型
+            for model in models:
+                model.train()
+                out = model(images)
+                model_loss = criterion(out, labels)
+                epoch_loss.append(model_loss.item())
+                model_loss.backward()
+            # 更新参数
+            optimizer.step()
+
+        epoch_accuracy = multi_check_accuracy(models, valid_loader, device)
+        if epoch_accuracy > best_accuracy:
+            best_accuracy = epoch_accuracy
+            for index, model in enumerate(models):
+                best_models[index] = deepcopy(model)
+        print(f'\n多模型集成第{epoch}个epoch训练完成, 损失为 {np.mean(epoch_loss):.4f}, 准确率为 {epoch_accuracy*100:.2f}%, 当前最高准确率为 {best_accuracy*100:.2f}%\n')
+
+    return best_models
+
+
 def main():
     # 超参数
     batch_size = 64
@@ -97,5 +156,35 @@ def main():
     print(f'最优模型测试准确率为：{test_best_accuracy*100:.2f}%')
 
 
+def multi_main():
+    # 超参数
+    batch_size = 64
+    learning_rate = 0.001
+    epochs = 50
+    num_train = 49000
+    device = specify_device(use_gpu=True)
+
+    # 模型集成
+    models = [CIFAR10Model().to(device), AdaptiveAvgCNNNet().to(device), LeNet().to(device)]
+    # 实例化模型 定义损失函数 定义优化器
+    criterion = nn.CrossEntropyLoss()
+    optimizer= optim.Adam([{"params":model.parameters()} for model in models],lr=learning_rate)
+
+    # 数据准备
+    train_loader = prepare_data('train', batch_size, num_train)
+    valid_loader = prepare_data('valid', batch_size, num_train)
+    test_loader = prepare_data('test', batch_size)
+
+    # 训练
+    best_models = multi_train(epochs, models, train_loader, valid_loader, optimizer, criterion, device)
+    print(f'\n{"-" * 30} 多模型集成训练完成 {"-" * 30}\n')
+    # 测试
+    test_best_accuracy = multi_check_accuracy(best_models, test_loader, device)
+    print(f'多模型集成最优模型最终测试准确率为：{test_best_accuracy*100:.2f}%')
+
+
 if __name__ == '__main__':
-    main()
+    # 普通单模型
+    # main()
+    # 多模型集成
+    multi_main()
--- scripts/CIFAR10/model.py
+++ scripts/CIFAR10/model.py
@@ -18,3 +18,77 @@ class CIFAR10Model(nn.Module):
         x = x.view(-1, 36*6*6)
         x = F.relu(self.fc2(F.relu(self.fc1(x))))
         return x
+
+
+class AdaptiveAvgCNNNet(nn.Module):
+    def __init__(self):
+        super(AdaptiveAvgCNNNet, self).__init__()
+        self.conv1 = nn.Conv2d(3, 16, 5)
+        self.pool1 = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(16, 36, 5)
+        #self.fc1 = nn.Linear(16 * 5 * 5, 120)
+        self.pool2 = nn.MaxPool2d(2, 2)
+        self.aap=nn.AdaptiveAvgPool2d(1)
+        #self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(36, 10)
+
+    def forward(self, x):
+        x = self.pool1(F.relu(self.conv1(x)))
+        x = self.pool2(F.relu(self.conv2(x)))
+        #print(x.shape)
+        #x = x.view(-1, 16 * 5 * 5)
+        x = self.aap(x)
+        #print(x.shape)
+        #x = F.relu(self.fc2(x))
+        x = x.view(x.shape[0], -1)
+        #print(x.shape)
+        x = self.fc3(x)
+        return x
+
+
+class LeNet(nn.Module):
+    def __init__(self):
+        super(LeNet, self).__init__()
+        self.conv1 = nn.Conv2d(3, 6, 5)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.fc1   = nn.Linear(16*5*5, 120)
+        self.fc2   = nn.Linear(120, 84)
+        self.fc3   = nn.Linear(84, 10)
+
+    def forward(self, x):
+        out = F.relu(self.conv1(x))
+        out = F.max_pool2d(out, 2)
+        out = F.relu(self.conv2(out))
+        out = F.max_pool2d(out, 2)
+        out = out.view(out.size(0), -1)
+        out = F.relu(self.fc1(out))
+        out = F.relu(self.fc2(out))
+        out = self.fc3(out)
+        return out
+
+
+class VGG(nn.Module):
+    def __init__(self, vgg_name):
+        super(VGG, self).__init__()
+        self.features = self._make_layers(cfg[vgg_name])
+        self.classifier = nn.Linear(512, 10)
+
+    def forward(self, x):
+        out = self.features(x)
+        out = out.view(out.size(0), -1)
+        out = self.classifier(out)
+        return out
+
+    def _make_layers(self, cfg):
+        layers = []
+        in_channels = 3
+        for x in cfg:
+            if x == 'M':
+                layers += [nn.MaxPool2d(kernel_size=2, stride=2)]
+            else:
+                layers += [nn.Conv2d(in_channels, x, kernel_size=3, padding=1),
+                            nn.BatchNorm2d(x),
+                            nn.ReLU(inplace=True)]
+                in_channels = x
+        layers += [nn.AvgPool2d(kernel_size=1, stride=1)]
+        return nn.Sequential(*layers)
