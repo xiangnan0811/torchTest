21799d5489d7fcad153ea31cc9ae738c228760c2
xiangnan0718 <xiangnan0718@gmail.com>
2021-11-15 11:11:12 +0800

    修复验证码数据集均值和标准差计算逻辑

--- scripts/letterCaptcha/calculateMean.py
+++ scripts/letterCaptcha/calculateMean.py
@@ -2,45 +2,46 @@ from tqdm import tqdm
 import numpy as np
 import os
 import cv2
+from torchvision import transforms as T
+from letterCaptchaDataset import LetterCaptchaDataset
 
 
 def calculate_mean_and_std(files_dir):
-    files = os.listdir(files_dir)
+    transform = T.Compose([
+        T.ToTensor(),
+    ])
+    captchas = LetterCaptchaDataset(files_dir, transform)
 
-    R, G, B = 0., 0., 0.
-    R_2, G_2, B_2 = 0., 0., 0.
-    N = 0
-    files = tqdm(files, total=len(files))
+    R_channel, G_channel, B_channel = 0., 0., 0.
+    R_channel_square, G_channel_square, B_channel_square = 0., 0., 0.
+    pixels_num = 0
 
-    for file in files:
-        img = cv2.imread(files_dir+file)
-        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        img = np.array(img)
-        h, w, c = img.shape
-        N += h*w
+    captchas = tqdm(captchas, total=len(captchas))
 
-        R_t = img[:, :, 0]
-        R += np.sum(R_t)
-        R_2 += np.sum(np.power(R_t, 2.0))
+    for image, _ in captchas:
+        img = image.permute(1, 2, 0).numpy()
+        h, w, _ = img.shape
+        pixels_num += h*w
 
-        G_t = img[:, :, 1]
-        G += np.sum(G_t)
-        G_2 += np.sum(np.power(R_t, 2.0))
+        R_temp = img[:, :, 0]
+        R_channel += np.sum(R_temp)
+        R_channel_square += np.sum(np.power(R_temp, 2.0))
 
-        B_t = img[:, :, 2]
-        B += np.sum(B_t)
-        B_2 += np.sum(np.power(R_t, 2.0))
+        G_temp = img[:, :, 1]
+        G_channel += np.sum(G_temp)
+        G_channel_square += np.sum(np.power(R_temp, 2.0))
 
-    R_mean, G_mean, B_mean = R/N, G/N, B/N
+        B_temp = img[:, :, 2]
+        B_channel += np.sum(B_temp)
+        B_channel_square += np.sum(np.power(R_temp, 2.0))
 
-    R_std = np.sqrt(R_2/N - R_mean*R_mean)
-    G_std = np.sqrt(G_2/N - G_mean*G_mean)
-    B_std = np.sqrt(B_2/N - B_mean*B_mean)
+    R_mean, G_mean, B_mean = R_channel/pixels_num, G_channel/pixels_num, B_channel/pixels_num
 
-    print("R_mean: %f, G_mean: %f, B_mean: %f" % (R_mean, G_mean, B_mean))
-    print("R_std: %f, G_std: %f, B_std: %f" % (R_std, G_std, B_std))
-    return (R_mean, G_mean, B_mean), (R_std, G_std, B_std)
+    R_std = np.sqrt(R_channel_square/pixels_num - R_mean*R_mean)
+    G_std = np.sqrt(G_channel_square/pixels_num - G_mean*G_mean)
+    B_std = np.sqrt(B_channel_square/pixels_num - B_mean*B_mean)
+
+    print(f'({R_mean:.4f}, {G_mean:.4f}, {B_mean:.4f}), ({B_std:.4f}, {R_std:.4f}, {G_std:.4f})')
 
 if __name__ == '__main__':
-    res = calculate_mean_and_std(files_dir='/Users/weibo/code/Python/torchTest/data/letter_captcha/test/')
-    print(res)
+    calculate_mean_and_std(files_dir='D:/code/deepLearning/torch/torchTest/data/letter_captcha/valid/')
